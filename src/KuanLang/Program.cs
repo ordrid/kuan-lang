aaa8c38275dd0bbcc3582a4fb420f56a7f06e363
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-04-04 21:09:24 +0800

    setup the beginnings of a REPL

--- src/KuanLang/Program.cs
+++ src/KuanLang/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+namespace KuanLang;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length > 1)
+        {
+            Console.WriteLine("Usage: kuan [script]");
+            Environment.Exit(64);
+        }
+        else if (args.Length == 1)
+        {
+            RunFile(args[0]);
+        }
+        else
+        {
+            RunPrompt();
+        }
+    }
+
+    static void RunFile(string path)
+    {
+        byte[] bytes = File.ReadAllBytes(path);
+        Run(Encoding.UTF8.GetString(bytes));
+    }
+
+    static void Run(string source)
+    {
+        var scanner = new Scanner { Source = source };
+        var tokens = scanner.ScanTokens();
+
+        foreach (var token in tokens)
+        {
+            Console.WriteLine(token);
+        }
+    }
+
+    static void RunPrompt()
+    {
+        var welcomeMessage = """
+
+             _  __                     _               _        _
+            | |/ /   _  __ _ _ __     | |__   __ _ ___| |_ __ _| |
+            | ' / | | |/ _` | '_ \    | '_ \ / _` / __| __/ _` | |
+            | . \ |_| | (_| | | | |_  | |_) | (_| \__ \ || (_| |_|  _ _ _
+            |_|\_\__,_|\__,_|_| |_( ) |_.__/ \__,_|___/\__\__,_(_) (_|_|_)
+                                  |/
+
+
+            """;
+
+        Console.WriteLine(welcomeMessage);
+
+        while (true)
+        {
+            Console.Write("kuan > ");
+
+            var line = Console.ReadLine();
+
+            if (line is null)
+            {
+                break;
+            }
+
+            Run(line);
+        }
+    }
+}
+
+sealed record Token { }
+
+sealed record Scanner
+{
+    public required string Source { get; init; }
+
+    public List<Token> ScanTokens()
+    {
+        Console.WriteLine(Source);
+        return [];
+    }
+}
